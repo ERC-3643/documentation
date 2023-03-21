a2d684e52e310e9a24a87ed23b806756bf2e3964
kevin.thizy <kevin.thizy@intech.lu>
2023-03-21 16:02:30 +0100

    🎉(init) Init documentation repo

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Developer documentation for the ERC3643 regulation token standard.">
+      <HomepageHeader />
+      <main>
+
+      </main>
+    </Layout>
+  );
+}
